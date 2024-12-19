4976911a7a489292d46c034304abadfb01918856
JuciusSea <haivip123badao@gmail.com>
2024-12-19 18:41:52 +0700

    RMI Object (chưa đúng lắm mà cũng tạm)

--- src/RMI/BookX.java
+++ src/RMI/BookX.java
@@ -17,4 +17,70 @@ public class BookX implements Serializable{
     private String author;
     private int yearPublished;
     private String genre;
+    private String code;
+    
+    public BookX(String id, String title, String author, int yearPublished, String genre){
+        this.id =id;
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.genre = genre;
+    }
+    public BookX(){
+        
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "BookX{" + "id=" + id + ", title=" + title + ", author=" + author + ", yearPublished=" + yearPublished + ", genre=" + genre + ", code=" + code + '}';
+    }
+    
 }
--- src/RMI/RMI_Object.java
+++ src/RMI/RMI_Object.java
@@ -4,10 +4,42 @@
  */
 package RMI;
 
+import RMI.ObjectService;
+import java.net.MalformedURLException;
+import java.rmi.*;
 /**
  *
  * @author haivi
  */
 public class RMI_Object {
+    public static String genCode(BookX book) {
+        
+        String author = book.getAuthor();
+        int yearPublished = book.getYearPublished();
+        String genre = book.getGenre();
+        String title = book.getTitle();
+        
+        String[] authorParts = author.trim().split(" ");
+        String firstLetter = String.valueOf(authorParts[0].charAt(0));
+        String lastLetter = String.valueOf(authorParts[authorParts.length - 1].charAt(authorParts[authorParts.length - 1].length() - 1));
+        
+        String yearLastletter = String.valueOf(yearPublished % 100);
+        String genreLength = String.valueOf(genre.length());
+        String titleLength = String.valueOf(title.length()%10);
+        
+        return firstLetter + lastLetter + yearLastletter + genreLength + titleLength;
+    }   
     
+    public static void main(String[] args) throws NotBoundException, MalformedURLException, RemoteException {
+        ObjectService service = (ObjectService) Naming.lookup("rmi://203.162.10.109/RMIObjectService");
+        
+        BookX book = (BookX) service.requestObject("B21DCCN317", "upRvuwLc");
+        
+        System.out.println(book);
+        
+        book.setCode(genCode(book));
+        System.out.println(book);
+        
+        service.submitObject("B21DCCN317","upRvuwLc", book);
+    }
 }
